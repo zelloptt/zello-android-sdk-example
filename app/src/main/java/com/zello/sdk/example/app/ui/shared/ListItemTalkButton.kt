239e3c1b9cd0e274ebd88dd0d7a69e20d6a34087
Anuj panchmia <164230466+anoooooj@users.noreply.github.com>
2025-06-09 09:57:37 -0500

    Updated the sdk version and fixed button behaviour on sample app. (#4)

--- app/src/main/java/com/zello/sdk/example/app/ui/shared/ListItemTalkButton.kt
+++ app/src/main/java/com/zello/sdk/example/app/ui/shared/ListItemTalkButton.kt
@@ -1,50 +1,57 @@
 package com.zello.sdk.example.app.ui.shared
 
-import android.view.MotionEvent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.zello.sdk.example.app.R
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun ListItemTalkButton(isEnabled: Boolean, isConnecting: Boolean, isTalking: Boolean, isReceiving: Boolean, onDown: () -> Unit, onUp: () -> Unit) {
-	var isPressed by remember { mutableStateOf(false) }
-	Button(onClick = {},
+fun ListItemTalkButton(
+	isEnabled: Boolean,
+	isConnecting: Boolean,
+	isTalking: Boolean,
+	isReceiving: Boolean,
+	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+	onDown: () -> Unit,
+	onUp: () -> Unit
+) {
+
+	LaunchedEffect(interactionSource) {
+		interactionSource.interactions.collect { interaction ->
+			when (interaction) {
+				is PressInteraction.Press -> {
+					onDown()
+				}
+				is PressInteraction.Release -> {
+					onUp()
+				}
+				is PressInteraction.Cancel -> {
+					onUp()
+				}
+				else -> {}
+			}
+		}
+	}
+
+	Button(
+		onClick = {},
 		enabled = isEnabled,
 		colors = ButtonDefaults.buttonColors(containerColor = if (isTalking) Color.Red else if (isReceiving) Color.Green else Color.Gray),
+		interactionSource = interactionSource,
 		modifier = Modifier
 			.padding(8.dp)
-			.pointerInteropFilter {
-				if (!isEnabled) {
-					return@pointerInteropFilter false
-				}
-				when (it.action) {
-					MotionEvent.ACTION_DOWN -> {
-						isPressed = true
-						onDown()
-						true
-					}
-					MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-						isPressed = false
-						onUp()
-						true
-					}
-					else -> false
-				}
-			}) {
+
+	) {
 		Text(text = stringResource(id = if (isConnecting) R.string.connecting else if (isTalking) R.string.talking else if (isReceiving) R.string.receiving else R.string.ptt))
 	}
 }
